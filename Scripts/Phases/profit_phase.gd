f161a74e95fa2f929eb6db9c95de89a1164ed664
WollyTech <ayolawole@gmail.com>
2025-01-25 16:36:40 +0000

    minor redirections"
    q
    :
    :wq
    /
    \
    exit

--- Scripts/Phases/profit_phase.gd
+++ Scripts/Phases/profit_phase.gd
@@ -1,22 +1,36 @@
-extends Node2D
+extends Node
 
-var time_limit = 3
-var current_time = 3
+var totalEarn: float
+var propEarn:float
+var stockEarn:float
+var totalSum:float
+
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	$Timer.start()
-	$TimerLabel.text = "Time Left: " + str(current_time)
+	pass # Replace with function body.
+
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
+	#run math as long as the timer is active
+	if(!$Timer/Timer.is_stopped()):
+		propEarn += 1
+		stockEarn += 1
+	
+		totalEarn = propEarn + stockEarn
+		
+		totalSum = Globals.money + totalEarn
+		
+		$TotalMoneyLabel.text = "Total Balance: $" + str(totalSum)
+		$TotalEarnLabel.text = "Total Earnings: $" + str(totalEarn)
+		$PropEarnLabel.text = "Property Earnings: $" + str(propEarn)
+		$StockEarnLabel.text = "Stock Earnings: $" + str(stockEarn)
+		$Timer/TimeLabel.text = "Time Left: " + str($Timer/Timer.time_left) + "s"		
 	pass
 
-# timer times out every 1 second, to get more accurate time counter - Tym
 func _on_timer_timeout() -> void:
-	current_time = current_time - 1
-	if (current_time == 0):
-			current_time = 30
-			get_tree().change_scene_to_file("res://Scenes/party_phase.tscn")
-	$TimerLabel.text = "Time Left: " + str(current_time)
-	$Timer.start()
+	Globals.money = totalSum
+	get_tree().change_scene_to_file("res://Scenes/party_phase.tscn")
+	pass # Replace with function body.
