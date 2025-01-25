1d74c03d655ae4ddb52b09e535b90a49cf00a4a7
Conor McDonagh Rollo <conormcdr@gmail.com>
2025-01-25 12:49:54 +0000

    Added stock screens & fixed folder structure

--- Scripts/UI/user_interface.gd
+++ Scripts/UI/user_interface.gd
@@ -34,3 +34,9 @@ func _on_profit_phase_button_pressed() -> void:
 
 func _on_property_phase_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://property_phase.tscn")
+	
+func _on_stock_market_button_pressed() -> void:
+	$StockMarket.visible = !$StockMarket.visible
+
+func _on_stock_inventory_button_pressed() -> void:
+	$StockInventory.visible = !$StockInventory.visible
