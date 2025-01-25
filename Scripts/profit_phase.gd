0e75ac0e601eeae50abaadc745e207465ea75b63
Olawole <ayolawole@gmail.com>
2025-01-25 14:33:41 +0000

    profit screen layout and loop

---
+++ Scripts/profit_phase.gd
@@ -0,0 +1,22 @@
+extends Node2D
+
+var time_limit = 3
+var current_time = 3
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	$Timer.start()
+	$TimerLabel.text = "Time Left: " + str(current_time)
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+# timer times out every 1 second, to get more accurate time counter - Tym
+func _on_timer_timeout() -> void:
+	current_time = current_time - 1
+	if (current_time == 0):
+			current_time = 30
+			get_tree().change_scene_to_file("res://party_phase.tscn")
+	$TimerLabel.text = "Time Left: " + str(current_time)
+	$Timer.start()
---
+++ Scripts/ui.gd
@@ -0,0 +1,33 @@
+extends Node
+
+var totalEarn: float
+var propEarn:float
+var stockEarn:float
+var totalSum:float
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	#run math as long as the timer is active
+	if(!$Timer/Timer.is_stopped()):
+		propEarn += 1
+		stockEarn += 1
+	
+		totalEarn = propEarn + stockEarn
+		
+		totalSum = Globals.money + totalEarn
+		
+		$TotalMoneyLabel.text = "Total Balance: $" + str(totalSum)
+		$TotalEarnLabel.text = "Total Earnings: $" + str(totalEarn)
+		$PropEarnLabel.text = "Property Earnings: $" + str(propEarn)
+		$StockEarnLabel.text = "Stock Earnings: $" + str(stockEarn)
+		$Timer/TimeLabel.text = "Time Left: " + str($Timer/Timer.time_left) + "s"		
+	pass
+
+func _on_timer_timeout() -> void:
+	Globals.money = totalSum
+	get_tree().change_scene_to_file("res://Scenes/party_phase.tscn")
+	pass # Replace with function body.
